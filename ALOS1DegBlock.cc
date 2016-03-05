8d5b93ee56d993e0cf25ef71101e78ee10bad857
Yifan Yu <yifany@ucla.edu>
2016-03-04 17:38:35 -0800

    new methods for ALOS1DegBlock

--- ALOS1DegBlock.cc
+++ ALOS1DegBlock.cc
@@ -2,8 +2,23 @@
 
 
 ALOS1DegBlock::ALOS1DegBlock(int latitude) {
+	if (latitude > 90 || latitude < -89)  {throw;}
+
+	//allocate heap space for the data block
+	const unsigned long long block_pixels = ALOS_BLOCK_XDIM * ALOS_TILE_YDIM
+	value_grid = new ALOS_TYPE[block_npixels]
+
+	//Read data from files into new grid
+	for (int i=-180, i<180, ++i) {
 
 
+
+	}
+
+}
+
+
+ALOS1DegBlock::~ALOS1DegBlock() {
 
 
 
@@ -11,8 +26,8 @@ ALOS1DegBlock::ALOS1DegBlock(int latitude) {
 }
 
 
-ALOS1DegBlock::~ALOS1DegBlock() {
-
+std::string ALOS1DegBlock::construct_tile_name(int longitude, int latitude) {
+	if(longitude > 179 || longitude < -180 || latitude > 90 || latitude < -89) {throw;}
 
 
 
